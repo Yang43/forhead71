325434dffa6331f1fbb299d42cece04727720c90
Yang43 <deardamn43@gmail.com>
2017-06-17 19:54:12 +0800

    Add files via upload

---
+++ TastMyFirstApp.java
@@ -0,0 +1,15 @@
+package Basic;
+
+import javax.swing.JFrame;
+
+public class TastMyFirstApp {
+public static void main(String[] args){
+	System.out.println("Hola");
+	JFrame f = new Jframe("title");
+	f.add(new Jbutton("Press Me");
+	f.setSize(300, 400);
+	f.setDefaultCloseOperation(JFrame.Exit_ON_Close);
+	f.setVisible(true);
+}
+}
+//
---
+++ TestFinalEX1.java
@@ -0,0 +1,8 @@
+package Basic;
+
+public class TestFinalEX1 {
+ public static void main(String args){
+final double pi = 0;
+System.out.println("pi" +pi);
+ }
+}
