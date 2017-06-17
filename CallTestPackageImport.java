958d30f855f120b5fe4823440c2e02707a3ad9af
Yang43 <deardamn43@gmail.com>
2017-06-17 19:29:57 +0800

    Add files via upload

---
+++ CallTestPackageImport.java
@@ -0,0 +1,10 @@
+package Mypackage;
+import TestPackageImpiortEx1;
+public class CallTestPackageImport {
+
+	public static void main(String[] args) {
+		TestPackageImpiortEx1 test1 = new TestPackageImportEx1();
+ test1.processAction("jump");
+	}
+
+}
